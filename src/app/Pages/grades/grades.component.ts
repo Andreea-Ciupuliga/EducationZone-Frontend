3b4df6eb614f3111c122b8356895d749d91450f8
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-03-28 22:17:42 +0300

    get all grades for a student

--- src/app/Pages/grades/grades.component.ts
+++ src/app/Pages/grades/grades.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {GetGradeDTO} from "../../DTOs/GradeDTOs/get-grade-dto";
+import {ParticipantsService} from "../../Services/ParticipantsService/participants.service";
 
 @Component({
   selector: 'app-grades',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GradesComponent implements OnInit {
 
-  constructor() { }
+  private studentId: number = 9;
+  public Grades: GetGradeDTO[] = [];
+  constructor(private readonly participantsService: ParticipantsService) { }
 
   ngOnInit(): void {
+    this.getAllGradesByStudentId();
+  }
+  getAllGradesByStudentId() {
+    this.participantsService.getAllGradesByStudentId(this.studentId).subscribe((data: GetGradeDTO[]) => {         //daca nu scriem .subscribe nu o sa se faca requestul
+      this.Grades = data;
+      console.log(this.Grades)
+    });
   }
 
 }
