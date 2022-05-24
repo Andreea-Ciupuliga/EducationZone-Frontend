6b41aee7d2220257bb8a4e3e0d3da4a602e782b3
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-05-25 01:01:24 +0300

    keycloak integration to get a student's username to display student homework

--- src/app/Pages/homeworks/homeworks.component.ts
+++ src/app/Pages/homeworks/homeworks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {GetHomeworkDTO} from "../../DTOs/HomeworkDTOs/get-homework-dto";
 import {HomeworkService} from "../../Services/HomeworkService/homework.service";
+import {KeycloakService} from "keycloak-angular";
 
 @Component({
   selector: 'app-homeworks',
@@ -8,15 +9,16 @@ import {HomeworkService} from "../../Services/HomeworkService/homework.service";
   styleUrls: ['./homeworks.component.scss']
 })
 export class HomeworksComponent implements OnInit {
-  private studentId: number = 9;
+  private studentUsername: string = "";
   public Homeworks: GetHomeworkDTO[] = [];
-  constructor(private readonly homeworkService:HomeworkService) { }
+  constructor(private readonly homeworkService:HomeworkService,private keycloakService: KeycloakService) { }
 
   ngOnInit(): void {
-    this.getAllHomeworksByStudentId();
+    this.getAllHomeworksByStudentUsername();
   }
-  getAllHomeworksByStudentId(){
-    this.homeworkService.getAllHomeworksByStudentId(this.studentId).subscribe((data: GetHomeworkDTO[]) => {         //daca nu scriem .subscribe nu o sa se faca requestul
+  getAllHomeworksByStudentUsername(){
+    this.studentUsername=this.keycloakService.getUsername();
+    this.homeworkService.getAllHomeworksByStudentUsername(this.studentUsername).subscribe((data: GetHomeworkDTO[]) => {
       this.Homeworks = data;
       console.log(this.Homeworks)
     });
