dc5f9acc8471a86e9aed093d76869a6e65c5a88f
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-05-25 21:16:32 +0300

    new imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,11 @@
 import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { OAuthModule } from 'angular-oauth2-oidc';
-//material imports
-import {MatDividerModule} from '@angular/material/divider';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatSelectModule} from '@angular/material/select';
-import {MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {HttpClientModule} from "@angular/common/http";
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatMenuModule} from '@angular/material/menu';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatInputModule} from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-//import {initializer} from "../utils/app-inits";
+import {initializer} from "../utils/app-inits";
 import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
-import { RegisterStudentComponent } from './Components/register-student/register-student.component';
-import { RegisterCourseComponent } from './Components/register-course/register-course.component';
-import { RegisterHomeworkComponent } from './Components/register-homework/register-homework.component';
-import { RegisterProfessorComponent } from './Components/register-professor/register-professor.component';
-import { RegisterExamComponent } from './Components/register-exam/register-exam.component';
-import { RemoveStudentComponent } from './Components/remove-student/remove-student.component';
-import { RemoveProfessorComponent } from './Components/remove-professor/remove-professor.component';
-import { RemoveCourseComponent } from './Components/remove-course/remove-course.component';
-import { RemoveExamComponent } from './Components/remove-exam/remove-exam.component';
-import { RemoveHomeworkComponent } from './Components/remove-homework/remove-homework.component';
+import { GetExamComponent } from './Components/get-exam/get-exam.component';
 import { HomeComponent } from './Pages/home/home.component';
 import { CourseDetailsComponent } from './Pages/course-details/course-details.component';
 import { CourseAdministrationComponent } from './Pages/course-administration/course-administration.component';
@@ -50,17 +23,44 @@ import { ProfessorAdministrationComponent } from './Pages/professor-administrati
 import { ToolbarComponent } from './Components/toolbar/toolbar.component';
 import { GradesComponent } from './Pages/grades/grades.component';
 import { GetStudentComponent } from './Components/get-student/get-student.component';
-import { AdminPageComponent } from './Pages/admin-page/admin-page.component';
 import { GetProfessorComponent } from './Components/get-professor/get-professor.component';
 import { GetCourseComponent } from './Components/get-course/get-course.component';
 import { GetHomeworkComponent } from './Components/get-homework/get-homework.component';
-import { ProfessorPageComponent } from './Pages/professor-page/professor-page.component';
 import { ManageCourseComponent } from './Pages/manage-course/manage-course.component';
+import {AccessDeniedComponent} from "./access-denied/access-denied.component";
+import {AdminHomeComponent} from "./Pages/admin-home/admin-home.component";
+import {ProfessorHomeComponent} from "./Pages/professor-home/professor-home.component";
+import {StudentHomeComponent} from "./Pages/student-home/student-home.component";
+import { EditProfileComponent } from './Pages/edit-profile/edit-profile.component';
+import { RegisterStudentComponent } from './Components/register-student/register-student.component';
+import { RegisterCourseComponent } from './Components/register-course/register-course.component';
+import { RegisterHomeworkComponent } from './Components/register-homework/register-homework.component';
+import { RegisterProfessorComponent } from './Components/register-professor/register-professor.component';
+import { RegisterExamComponent } from './Components/register-exam/register-exam.component';
+import { RemoveStudentComponent } from './Components/remove-student/remove-student.component';
+import { RemoveProfessorComponent } from './Components/remove-professor/remove-professor.component';
+import { RemoveCourseComponent } from './Components/remove-course/remove-course.component';
+import { RemoveExamComponent } from './Components/remove-exam/remove-exam.component';
+import { RemoveHomeworkComponent } from './Components/remove-homework/remove-homework.component';
+
+//material imports
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {HttpClientModule} from "@angular/common/http";
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 import {ToastrModule} from "ngx-toastr";
-import { GetExamComponent } from './Components/get-exam/get-exam.component';
-//import {AccessDeniedComponent} from "./access-denied/access-denied.component";
-//import {ManagerComponent} from "./manager/manager.component";
-//import {AdminComponent} from "./admin/admin.component";
+
 
 
 @NgModule({
@@ -93,17 +93,17 @@ import { GetExamComponent } from './Components/get-exam/get-exam.component';
     ToolbarComponent,
     GradesComponent,
     GetStudentComponent,
-    AdminPageComponent,
+    AdminHomeComponent,
     GetProfessorComponent,
     GetCourseComponent,
     GetHomeworkComponent,
-    ProfessorPageComponent,
+    ProfessorHomeComponent,
     ManageCourseComponent,
     GetExamComponent,
+    StudentHomeComponent,
+    AccessDeniedComponent,
+    EditProfileComponent,
 
-    //AccessDeniedComponent,
-    //ManagerComponent,
-    //AdminComponent,
   ],
   imports: [
     MatDividerModule,
@@ -128,13 +128,13 @@ import { GetExamComponent } from './Components/get-exam/get-exam.component';
     ToastrModule.forRoot(),
 
   ],
-  providers: [// add this provider
-    // {
-    //   provide: APP_INITIALIZER,
-    //   useFactory: initializer,
-    //   deps: [KeycloakService],
-    //   multi: true,
-    // },
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializer,
+      deps: [KeycloakService],
+      multi: true,
+    },
   ],
   bootstrap: [AppComponent]
 })
