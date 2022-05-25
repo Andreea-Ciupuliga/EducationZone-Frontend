30262df6be81c8a56b662353c1ed57f57350be6c
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-05-25 21:11:08 +0300

    add student home page

---
+++ src/app/Pages/student-home/student-home.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {GetCourseDTO} from "../../DTOs/CourseDTOs/get-course-dto";
+import {ParticipantsService} from "../../Services/ParticipantsService/participants.service";
+import {KeycloakService} from "keycloak-angular";
+
+@Component({
+  selector: 'app-student-home',
+  templateUrl: './student-home.component.html',
+  styleUrls: ['./student-home.component.scss']
+})
+export class StudentHomeComponent implements OnInit {
+
+  public Courses: GetCourseDTO[] = [];
+  private studentUsername: string = "";
+
+  constructor(private readonly participantsService: ParticipantsService, private keycloakService: KeycloakService) {
+  }
+
+  ngOnInit(): void {
+
+    this.getAllCoursesByStudentUsername();
+  }
+
+  getAllCoursesByStudentUsername() {
+
+    this.studentUsername = this.keycloakService.getUsername();
+    this.participantsService.getAllCoursesByStudentUsername(this.studentUsername).subscribe((data: GetCourseDTO[]) => {
+      this.Courses = data;
+      console.log(this.Courses)
+    });
+
+  }
+
+}
