1208b1b300af09b5d3eb35c3a8d5fc463fc1c924
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-05-25 21:18:25 +0300

    integrate keycloak

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  serverUrl:'http://localhost:8081',
+  keycloak: {
+    // Url of the Identity Provider
+    issuer: 'http://localhost:8080/auth/',
+    // Realm
+    realm: 'Education-Zone',
+    clientId: 'Education-zone-client-frontend',
+  },
 };
 
 /*
