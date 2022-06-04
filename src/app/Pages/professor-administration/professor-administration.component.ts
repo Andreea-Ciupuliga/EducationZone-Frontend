ea311a1c9b5114f450f0438ac58a0ee59e56545d
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-06-05 00:33:39 +0300

    new style for professor administration

--- src/app/Pages/professor-administration/professor-administration.component.ts
+++ src/app/Pages/professor-administration/professor-administration.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {RegisterStickyNotesComponent} from "../../Components/register-sticky-notes/register-sticky-notes.component";
+import {MatDialog} from "@angular/material/dialog";
+import {RegisterProfessorComponent} from "../../Components/register-professor/register-professor.component";
+import {UpdateCourseComponent} from "../../Components/update-course/update-course.component";
+import {RemoveCourseComponent} from "../../Components/remove-course/remove-course.component";
+import {UpdateProfessorComponent} from "../../Components/update-professor/update-professor.component";
+import {RemoveProfessorComponent} from "../../Components/remove-professor/remove-professor.component";
 
 @Component({
   selector: 'app-professor-administration',
@@ -7,9 +14,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfessorAdministrationComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+  openDialogRegisterProfessor() {
+    const dialogRef = this.dialog.open(RegisterProfessorComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 
+  openDialogUpdateProfessor() {
+    const dialogRef = this.dialog.open(UpdateProfessorComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogRemoveProfessor() {
+    const dialogRef = this.dialog.open(RemoveProfessorComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
