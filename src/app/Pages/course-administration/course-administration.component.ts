d51cc9870323f0138e57e5e1f81dd7212051a6cc
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-06-05 00:32:39 +0300

    new style for course administration

--- src/app/Pages/course-administration/course-administration.component.ts
+++ src/app/Pages/course-administration/course-administration.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {RegisterCourseComponent} from "../../Components/register-course/register-course.component";
+import {UpdateCourseComponent} from "../../Components/update-course/update-course.component";
+import {RemoveCourseComponent} from "../../Components/remove-course/remove-course.component";
 
 @Component({
   selector: 'app-course-administration',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseAdministrationComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  openDialogAddNewCourse() {
+    const dialogRef = this.dialog.open(RegisterCourseComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogUpdateCourse() {
+    const dialogRef = this.dialog.open(UpdateCourseComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogRemoveCourse() {
+    const dialogRef = this.dialog.open(RemoveCourseComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 
 }
