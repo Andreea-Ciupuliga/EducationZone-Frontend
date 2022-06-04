97cd19baf26b26f900a5c97798acbbf3c498f730
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-06-05 00:28:40 +0300

    change update course to display in the dialog box

--- src/app/Components/update-course/update-course.component.ts
+++ src/app/Components/update-course/update-course.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
 import {CourseService} from "../../Services/CourseService/course.service";
-import {StudentService} from "../../Services/StudentService/student.service";
 import {NotificationService} from "../../Services/NotificationService/notification.service";
 
 @Component({
@@ -13,12 +12,11 @@ export class UpdateCourseComponent implements OnInit {
 
   registrationForm = this.fb.group({
     id: [null, Validators.required],
-    name: [null, Validators.required],
-    numberOfStudents: [null, Validators.required],
-    description: [null, Validators.required],
-    year: [null, Validators.required],
-    semester: [null, Validators.required],
-    professorId: [null, Validators.required],
+    name: [null],
+    description: [null],
+    year: [null],
+    semester: [null],
+    professorId: [null],
   })
 
   constructor(private readonly courseService: CourseService, private fb: FormBuilder, private notifyService: NotificationService) {
