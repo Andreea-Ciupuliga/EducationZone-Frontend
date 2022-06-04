684ffdc19e1fc607abfd65fe0b379ccec6601784
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-06-05 00:32:58 +0300

    new style for exam administration

--- src/app/Pages/exam-administration/exam-administration.component.ts
+++ src/app/Pages/exam-administration/exam-administration.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {RegisterCourseComponent} from "../../Components/register-course/register-course.component";
+import {UpdateCourseComponent} from "../../Components/update-course/update-course.component";
+import {RemoveCourseComponent} from "../../Components/remove-course/remove-course.component";
+import {RegisterExamComponent} from "../../Components/register-exam/register-exam.component";
+import {UpdateExamComponent} from "../../Components/update-exam/update-exam.component";
+import {RemoveExamComponent} from "../../Components/remove-exam/remove-exam.component";
 
 @Component({
   selector: 'app-exam-administration',
@@ -7,9 +14,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExamAdministrationComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+  openDialogAddNewExam() {
+    const dialogRef = this.dialog.open(RegisterExamComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 
+  openDialogUpdateExam() {
+    const dialogRef = this.dialog.open(UpdateExamComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogRemoveExam() {
+    const dialogRef = this.dialog.open(RemoveExamComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
