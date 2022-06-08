3f16ab1a196fe126ef94a7dd3071730c69bacc36
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-06-08 19:19:43 +0300

    Improved interface for manage course page

--- src/app/Pages/manage-course/manage-course.component.ts
+++ src/app/Pages/manage-course/manage-course.component.ts
@@ -13,6 +13,15 @@ import {NotificationService} from "../../Services/NotificationService/notificati
 import {Exam} from "../../Models/exam";
 import {Homework} from "../../Models/homework";
 import {Participants} from "../../Models/participants";
+import {RegisterExamComponent} from "../../Components/register-exam/register-exam.component";
+import {MatDialog} from "@angular/material/dialog";
+import {RegisterExamWithoutCourseIdComponent} from "../../Components/register-exam-without-course-id/register-exam-without-course-id.component";
+import {RegisterHomeworkWithoutCourseIdComponent} from "../../Components/register-homework-without-course-id/register-homework-without-course-id.component";
+import {UpdateExamWithoutExamIdComponent} from "../../Components/update-exam-without-exam-id/update-exam-without-exam-id.component";
+import {UpdateCourseWithoutCourseIdComponent} from "../../Components/update-course-without-course-id/update-course-without-course-id.component";
+import {UpdateHomeworkWithoutHomeworkIdComponent} from "../../Components/update-homework-without-homework-id/update-homework-without-homework-id.component";
+import {RegisterGradeToStudentComponent} from "../../Components/register-grade-to-student/register-grade-to-student.component";
+import {GetStudentAndGradeDTO} from "../../DTOs/StudentDTOs/get-student-and-grade-dto";
 
 @Component({
   selector: 'app-manage-course',
@@ -20,58 +29,22 @@ import {Participants} from "../../Models/participants";
   styleUrls: ['./manage-course.component.scss']
 })
 export class ManageCourseComponent implements OnInit {
+  displayedColumnsHomeworks: string[] = ['deadline', 'points', 'description', 'removeHomework', 'editHomework'];
+  displayedColumnsStudents: string[] = ['id', 'firstName', 'lastName','email', 'username','groupNumber','phone','year','department','grade','addGrade'];
+
 
   public Homeworks: GetHomeworkDTO[] = [];
   public course: GetCourseDTO;
   public exam: GetExamDTO;
 
-  public AllStudents: GetStudentDTO[] = [];
+  public AllStudents: GetStudentAndGradeDTO[] = [];
   panelOpenState = false;
 
   addGradeForStudentForm = this.fb.group({
     courseGrade: ['', Validators.required]
   });
 
-  registrationFormHomework = this.fb.group({
-    description: ['', Validators.required],
-    deadline: ['', Validators.required],
-    points: ['', Validators.required],
-  })
-
-  registrationFormExam = this.fb.group({
-    description: ['', Validators.required],
-    examDate: ['', Validators.required],
-    points: ['', Validators.required],
-    examRoom: [null, Validators.required],
-    examHour: [null, Validators.required],
-  })
-
-  updateFormExam = this.fb.group({
-    description: [null, Validators.required],
-    examDate: [null, Validators.required],
-    points: [null, Validators.required],
-    examRoom: [null, Validators.required],
-    examHour: [null, Validators.required],
-  })
-
-  updateFormCourse = this.fb.group({
-    name: [null, Validators.required],
-    numberOfStudents: [null, Validators.required],
-    description: [null, Validators.required],
-    year: [null, Validators.required],
-    semester: [null, Validators.required],
-  })
-
-  updateFormHomework = this.fb.group({
-    id: [null, Validators.required],
-    description: [null, Validators.required],
-    deadline: [null, Validators.required],
-    points: [null, Validators.required],
-    courseId: [null, Validators.required],
-
-  })
-
-  constructor(private readonly participantsService: ParticipantsService, private readonly examService: ExamService, private readonly homeworkService: HomeworkService, private readonly courseService: CourseService, private route: ActivatedRoute, private fb: FormBuilder, private notifyService: NotificationService) {
+  constructor(public dialog: MatDialog, private readonly participantsService: ParticipantsService, private readonly examService: ExamService, private readonly homeworkService: HomeworkService, private readonly courseService: CourseService, private route: ActivatedRoute, private fb: FormBuilder, private notifyService: NotificationService) {
   }
 
   ngOnInit(): void {
@@ -86,76 +59,76 @@ export class ManageCourseComponent implements OnInit {
 
     this.examService.getExamByCourseId(Number(this.route.snapshot.paramMap.get('id'))).subscribe((data: GetExamDTO) => {
       this.exam = data;
-      console.log(this.exam);
-    });
-
-  }
-
-  updateCourse(): void {
-
-    let courseId = Number(this.route.snapshot.paramMap.get('id'));
-    let courseRegisterDto = this.updateFormCourse.value;
-    this.updateFormCourse.reset();
-    this.courseService.updateCourse(courseId, courseRegisterDto).subscribe((data: any) => {
-    }, (err) => {
-      this.notifyService.showError(err.error.message);
-    });
-
-  }
-
-  updateExam(): void {
-    let examRegisterDto = this.updateFormExam.value;
-    examRegisterDto.courseId = Number(this.route.snapshot.paramMap.get('id'));
-    this.updateFormExam.reset();
-    this.examService.updateExamByCourseId(examRegisterDto).subscribe((data: any) => {
-    }, (err) => {
-      this.notifyService.showError(err.error.message);
-    });
-  }
-
-  updateHomework(): void {
-    let homeworkId = this.updateFormHomework.value.id;
-    let homeworkRegisterDto = this.updateFormHomework.value;
-    this.updateFormHomework.reset();
-    this.homeworkService.updateHomework(homeworkId, homeworkRegisterDto).subscribe((data: any) => {
-    }, (err) => {
-      this.notifyService.showError(err.error.message);
     });
 
   }
 
   getAllStudentsByCourseId() {
     let courseId = Number(this.route.snapshot.paramMap.get('id'));
-    this.participantsService.getAllStudentsByCourseId(courseId).subscribe((data: GetStudentDTO[]) => {
+    this.participantsService.getAllStudentsAndGradesByCourseId(courseId).subscribe((data: GetStudentAndGradeDTO[]) => {
       this.AllStudents = data;
+      console.log(this.AllStudents)
     });
   }
 
-  registerHomework(): void {
-
-    let homeworkRegisterDto = this.registrationFormHomework.value;
-    homeworkRegisterDto.courseId = Number(this.route.snapshot.paramMap.get('id'));
-    this.registrationFormHomework.reset();
-    this.homeworkService.registerHomework(homeworkRegisterDto).subscribe((data: any) => {
+  openDialogAddNewExam() {
+    const dialogRef = this.dialog.open(RegisterExamWithoutCourseIdComponent, {
+      data: {
+        courseId: Number(this.route.snapshot.paramMap.get('id'))
+      }
     });
-
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 
-  registerExam(): void {
-
-    let examRegisterDto = this.registrationFormExam.value;
-    examRegisterDto.courseId = Number(this.route.snapshot.paramMap.get('id'));
-    this.registrationFormExam.reset();
-    this.examService.registerExam(examRegisterDto).subscribe((data: any) => {
-      },
-      (err) => {
-        this.notifyService.showError(err.error.message);
-      });
-
+  openDialogAddNewHomework() {
+    const dialogRef = this.dialog.open(RegisterHomeworkWithoutCourseIdComponent, {
+      data: {
+        courseId: Number(this.route.snapshot.paramMap.get('id'))
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
+
+  openDialogUpdateExam(id:number) {
+    const dialogRef = this.dialog.open(UpdateExamWithoutExamIdComponent, {
+      data: {
+        examId:id
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogUpdateCourse() {
+    const dialogRef = this.dialog.open(UpdateCourseWithoutCourseIdComponent, {
+      data: {
+        courseId: Number(this.route.snapshot.paramMap.get('id'))
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  openDialogUpdateHomework(id: number) {
+    const dialogRef = this.dialog.open(UpdateHomeworkWithoutHomeworkIdComponent, {
+      data: {
+        homeworkId: id
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
 
   removeExam(id: number) {
-    window.location.reload()
+
     this.examService.removeExam(id).subscribe((data: Exam) => {
     }, (err) => {
       this.notifyService.showError(err.error.message);
@@ -163,21 +136,23 @@ export class ManageCourseComponent implements OnInit {
   }
 
   removeHomework(id: number) {
-    window.location.reload()
+
     this.homeworkService.removeHomework(id).subscribe((data: Homework) => {
     }, (err) => {
       this.notifyService.showError(err.error.message);
     });
   }
 
-  addGradeForStudent(studentId: number, courseGrade: string) {
-
-    let courseId = Number(this.route.snapshot.paramMap.get('id'));
-    this.participantsService.addGradeForStudent(studentId, courseId, courseGrade).subscribe((data: Participants) => {
-    }, (err) => {
-      this.notifyService.showError(err.error.message);
+  openDialogAddGradeForStudent(id:number){
+    const dialogRef = this.dialog.open(RegisterGradeToStudentComponent, {
+      data: {
+        courseId: Number(this.route.snapshot.paramMap.get('id')),
+        studentId: id
+      }
     });
-
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 
 }
