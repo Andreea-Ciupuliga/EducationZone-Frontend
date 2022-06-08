20fdd9ed2acdee54d0dcd3ed2659f33c5041ec85
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-06-08 18:23:18 +0300

    Improved interface for homeworks component

--- src/app/Pages/homeworks/homeworks.component.ts
+++ src/app/Pages/homeworks/homeworks.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {GetHomeworkDTO} from "../../DTOs/HomeworkDTOs/get-homework-dto";
 import {HomeworkService} from "../../Services/HomeworkService/homework.service";
 import {KeycloakService} from "keycloak-angular";
+import {GetExamDTO} from "../../DTOs/ExamDTOs/get-exam-dto";
 
 @Component({
   selector: 'app-homeworks',
@@ -9,8 +10,15 @@ import {KeycloakService} from "keycloak-angular";
   styleUrls: ['./homeworks.component.scss']
 })
 export class HomeworksComponent implements OnInit {
+  panelOpenState = false;
+  displayedColumns: string[] = ['courseName','deadline','points', 'description'];
+
+  public courseName: string;
   private studentUsername: string = "";
+
   public Homeworks: GetHomeworkDTO[] = [];
+  public AllHomeworksByCourseName: GetHomeworkDTO[] = [];
+
   constructor(private readonly homeworkService:HomeworkService,private keycloakService: KeycloakService) { }
 
   ngOnInit(): void {
@@ -21,6 +29,15 @@ export class HomeworksComponent implements OnInit {
     this.homeworkService.getAllHomeworksByStudentUsername(this.studentUsername).subscribe((data: GetHomeworkDTO[]) => {
       this.Homeworks = data;
       console.log(this.Homeworks)
+    });
+  }
+
+  getAllHomeworksByCourseNameAndStudentUsername(name: string) {
+    this.courseName = "";
+    this.studentUsername = this.keycloakService.getUsername();
+
+    this.homeworkService.getAllHomeworksByCourseNameAndStudentUsername(name, this.studentUsername).subscribe((data: GetHomeworkDTO[]) => {
+      this.AllHomeworksByCourseName = data;
     });
   }
 }
