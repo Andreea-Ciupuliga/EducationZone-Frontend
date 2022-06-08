dcdfb9b05e66ed348c63bf497526e95d058f3f5a
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-06-08 18:22:45 +0300

    Improved interface for exams component

--- src/app/Pages/exams/exams.component.ts
+++ src/app/Pages/exams/exams.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ExamService} from "../../Services/ExamService/exam.service";
 import {GetExamDTO} from "../../DTOs/ExamDTOs/get-exam-dto";
 import {KeycloakService} from "keycloak-angular";
@@ -9,17 +9,36 @@ import {KeycloakService} from "keycloak-angular";
   styleUrls: ['./exams.component.scss']
 })
 export class ExamsComponent implements OnInit {
+  displayedColumns: string[] = ['courseName', 'examDate', 'examHour', 'examRoom', 'points', 'description'];
+  panelOpenState = false;
+
   private studentUsername: string = "";
+  public courseName: string;
+
   public Exams: GetExamDTO[] = [];
-  constructor(private readonly examService: ExamService,private keycloakService: KeycloakService) { }
+  public AllExamsByCourseName: GetExamDTO[] = [];
+
+  constructor(private readonly examService: ExamService, private keycloakService: KeycloakService) {
+  }
 
   ngOnInit(): void {
     this.getAllExamsByStudentUsername();
   }
+
   getAllExamsByStudentUsername() {
-    this.studentUsername=this.keycloakService.getUsername();
+    this.studentUsername = this.keycloakService.getUsername();
     this.examService.getAllExamsByStudentUsername(this.studentUsername).subscribe((data: GetExamDTO[]) => {
       this.Exams = data;
+      console.log(this.Exams)
+    });
+  }
+
+  getAllExamsByCourseNameAndStudentUsername(name: string) {
+    this.courseName = "";
+    this.studentUsername = this.keycloakService.getUsername();
+
+    this.examService.getAllExamsByCourseNameAndStudentUsername(name, this.studentUsername).subscribe((data: GetExamDTO[]) => {
+      this.AllExamsByCourseName = data;
       console.log(this.Exams)
     });
   }
