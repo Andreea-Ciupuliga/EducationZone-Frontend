b96a4d5e1bdf1cd6cb37780f42c20b48d80dd6cf
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-06-25 23:02:17 +0300

    edited to block students from accessing other courses they do not attend

--- src/app/Pages/course-details/course-details.component.ts
+++ src/app/Pages/course-details/course-details.component.ts
@@ -1,11 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {GetCourseDTO} from "../../DTOs/CourseDTOs/get-course-dto";
 import {CourseService} from "../../Services/CourseService/course.service";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {HomeworkService} from "../../Services/HomeworkService/homework.service";
 import {GetHomeworkDTO} from "../../DTOs/HomeworkDTOs/get-homework-dto";
 import {ExamService} from "../../Services/ExamService/exam.service";
 import {GetExamDTO} from "../../DTOs/ExamDTOs/get-exam-dto";
+import {ParticipantsService} from "../../Services/ParticipantsService/participants.service";
+import {KeycloakService} from "keycloak-angular";
 
 @Component({
   selector: 'app-course-details',
@@ -14,9 +16,9 @@ import {GetExamDTO} from "../../DTOs/ExamDTOs/get-exam-dto";
 })
 export class CourseDetailsComponent implements OnInit {
 
-  displayedColumns: string[] = ['deadline','points', 'description'];
+  displayedColumns: string[] = ['deadline', 'points', 'description'];
   public Homeworks: GetHomeworkDTO[] = [];
-  public course: GetCourseDTO={
+  public course: GetCourseDTO = {
     id: 0,
     name: "",
     numberOfStudents: 0,
@@ -27,10 +29,12 @@ export class CourseDetailsComponent implements OnInit {
   }
   public exam: GetExamDTO;
 
-  constructor(private readonly examService: ExamService, private readonly homeworkService: HomeworkService, private readonly courseService: CourseService, private route: ActivatedRoute) {
+  constructor(protected readonly router: Router, protected readonly keycloakService: KeycloakService, private readonly participantsService: ParticipantsService, private readonly examService: ExamService, private readonly homeworkService: HomeworkService, private readonly courseService: CourseService, private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
+
+    this.isAccessAllowed();
 
     this.courseService.getCourse(Number(this.route.snapshot.paramMap.get('id'))).subscribe((data: GetCourseDTO) => {
       this.course = data;
@@ -52,6 +56,13 @@ export class CourseDetailsComponent implements OnInit {
         console.warn(error);
       });
 
+  }
+
+  public async isAccessAllowed() {
+    this.participantsService.checkIfTheStudentIsAddedToTheCourse(Number(this.route.snapshot.paramMap.get('id')), this.keycloakService.getUsername()).subscribe((data: Boolean) => {
+      if (data == false)
+        this.router.navigate(['access-denied']);
+    });
   }
 
 }
