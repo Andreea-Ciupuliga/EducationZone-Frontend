96eebed0b06cdcca82094d1a2664c4bd8b722ded
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-06-25 23:00:50 +0300

    edit to refresh the component

--- src/app/Pages/sticky-notes/sticky-notes.component.ts
+++ src/app/Pages/sticky-notes/sticky-notes.component.ts
@@ -1,14 +1,10 @@
-import {Component, Injectable, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {GetStickyNoteDto} from "../../DTOs/StickyNoteDTOs/get-sticky-note-dto";
 import {StickyNoteService} from "../../Services/StickyNoteService/sticky-note.service";
 import {KeycloakService} from "keycloak-angular";
 import {MatDialog} from "@angular/material/dialog";
 import {RegisterStickyNotesComponent} from "../../Components/register-sticky-notes/register-sticky-notes.component";
-import {Homework} from "../../Models/homework";
-import {StickyNote} from "../../Models/sticky-note";
-import {FormBuilder} from "@angular/forms";
-import {NotificationService} from "../../Services/NotificationService/notification.service";
-import {NavigationEnd, Router} from "@angular/router";
+import {StickyNote} from "../../Model/sticky-note";
 
 @Component({
   selector: 'app-sticky-notes',
@@ -20,33 +16,32 @@ export class StickyNotesComponent implements OnInit {
   private studentUsername: string = "";
   public stickyNotes: GetStickyNoteDto[] = [];
 
-  constructor(private router: Router,public dialog: MatDialog,private readonly stickyNoteService: StickyNoteService,private keycloakService: KeycloakService, private notifyService: NotificationService) {
+  constructor(public dialog: MatDialog, private readonly stickyNoteService: StickyNoteService, private keycloakService: KeycloakService) {
   }
 
   ngOnInit(): void {
-
-
-    this.studentUsername=this.keycloakService.getUsername();
+    this.studentUsername = this.keycloakService.getUsername();
     this.stickyNoteService.getAllStickyNotesByStudentUsername(this.studentUsername).subscribe((data: GetStickyNoteDto[]) => {
       this.stickyNotes = data;
-      console.log(this.stickyNotes)
     });
   }
 
   openDialog() {
     const dialogRef = this.dialog.open(RegisterStickyNotesComponent);
     dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-      this.ngOnInit();
+
+      this.stickyNoteService.getAllStickyNotesByStudentUsername(this.studentUsername).subscribe((data: GetStickyNoteDto[]) => {
+        this.stickyNotes = data;
+      });
     });
+
   }
 
   removeStickyNote(id: number) {
-
     this.stickyNoteService.removeStickyNote(id).subscribe((data: StickyNote) => {
-      this.ngOnInit();
-    }, (err) => {
-      this.notifyService.showError(err.error.message);
+      this.stickyNoteService.getAllStickyNotesByStudentUsername(this.studentUsername).subscribe((data: GetStickyNoteDto[]) => {
+        this.stickyNotes = data;
+      });
     });
   }
 
