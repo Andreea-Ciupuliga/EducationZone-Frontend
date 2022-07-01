8fab2d8a533299e615eb2539321175ea238cdef4
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-07-01 19:25:23 +0300

    add dialog box for when I delete a professor

--- src/app/Components/get-professor/get-professor.component.ts
+++ src/app/Components/get-professor/get-professor.component.ts
@@ -2,12 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {GetProfessorDTO} from "../../DTOs/ProfessorDTOs/get-professor-dto";
 import {ProfessorService} from "../../Services/ProfessorService/professor.service";
 import {NotificationService} from "../../Services/NotificationService/notification.service";
-import {Professor} from "../../Model/professor";
-import {UpdateExamWithoutExamIdComponent} from "../update-exam-without-exam-id/update-exam-without-exam-id.component";
 import {MatDialog} from "@angular/material/dialog";
 import {UpdateProfessorWithoutProfessorIdComponent} from "../update-professor-without-professor-id/update-professor-without-professor-id.component";
-import {GetStudentDTO} from "../../DTOs/StudentDTOs/get-student-dto";
 import {MatTableDataSource} from "@angular/material/table";
+import {RemoveProfessorConfirmationDialogComponent} from "../remove-professor-confirmation-dialog/remove-professor-confirmation-dialog.component";
 
 @Component({
   selector: 'app-get-professor',
@@ -26,7 +24,7 @@ export class GetProfessorComponent implements OnInit {
   public professor: GetProfessorDTO;
 
   public dataSourceAllProfessors = new MatTableDataSource<GetProfessorDTO>();
-  public dataSourceProfessorsByName= new MatTableDataSource<GetProfessorDTO>();
+  public dataSourceProfessorsByName = new MatTableDataSource<GetProfessorDTO>();
   private oldProfessor: GetProfessorDTO;
 
   constructor(public dialog: MatDialog, private readonly professorService: ProfessorService, private notifyService: NotificationService) {
@@ -38,6 +36,7 @@ export class GetProfessorComponent implements OnInit {
   }
 
 //=======================================getAllProfessorsByName========================================================================================================
+
   getAllProfessorsByName(name: string) {
     this.professorName = name;
     this.professorService.getAllProfessorsByName(name).subscribe((data: GetProfessorDTO[]) => {
@@ -46,25 +45,25 @@ export class GetProfessorComponent implements OnInit {
       },
       (err) => {
         this.notifyService.showError(err.error.message);
+        this.dataSourceProfessorsByName.data = [];
       }
     );
   }
 
-  removeProfessorForGetAllProfessorsByNameFunction(id: number) {
-    this.professorService.removeProfessor(id).subscribe((data: Professor) => {
-
-
-
-      this.professorService.getAllProfessorsByName( this.professorName).subscribe((data: GetProfessorDTO[]) => {
+  openDialogRemoveProfessorForGetAllProfessorsByNameFunction(id: number) {
+    const dialogRef = this.dialog.open(RemoveProfessorConfirmationDialogComponent, {
+      data: {
+        professorId: id,
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      this.professorService.getAllProfessorsByName(this.professorName).subscribe((data: GetProfessorDTO[]) => {
           this.ProfessorsByName = data;
           this.dataSourceProfessorsByName.data = this.ProfessorsByName;
         },
         (err) => {
           this.dataSourceProfessorsByName.data = [];
-        }
-      );
-
-
+        });
     });
   }
 
@@ -75,28 +74,20 @@ export class GetProfessorComponent implements OnInit {
       }
     });
     dialogRef.afterClosed().subscribe(result => {
-
-
       this.professorService.getProfessor(id).subscribe((data: GetProfessorDTO) => {
-
-          const professor = this.ProfessorsByName.find(professor => professor.id == id)
-
-          if (professor) {
-            this.oldProfessor = professor;
-            var index = this.ProfessorsByName.indexOf(this.oldProfessor)
-            this.ProfessorsByName[index] = data;
-            this.dataSourceProfessorsByName.data = this.ProfessorsByName;
-          }
-
-        });
-
-
-
-
+        const professor = this.ProfessorsByName.find(professor => professor.id == id)
+        if (professor) {
+          this.oldProfessor = professor;
+          var index = this.ProfessorsByName.indexOf(this.oldProfessor)
+          this.ProfessorsByName[index] = data;
+          this.dataSourceProfessorsByName.data = this.ProfessorsByName;
+        }
+      });
     });
   }
 
 //======================================getProfessor=========================================================================================================
+
   getProfessor(id: number) {
     this.professorId = "";
     this.professorService.getProfessor(id).subscribe((data: GetProfessorDTO) => {
@@ -104,13 +95,24 @@ export class GetProfessorComponent implements OnInit {
 
     }, (err) => {
       this.notifyService.showError(err.error.message);
+      // @ts-ignore
+      this.professor = null;
     });
   }
 
-  removeProfessorForGetProfessorFunction(id: number) {
-    this.professorService.removeProfessor(id).subscribe((data: Professor) => {
-      // @ts-ignore
-      this.professor=null;
+  openDialogRemoveProfessorForGetProfessorFunction(id: number) {
+    const dialogRef = this.dialog.open(RemoveProfessorConfirmationDialogComponent, {
+      data: {
+        professorId: id,
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      this.professorService.getProfessor(id).subscribe((data: GetProfessorDTO) => {
+        this.professor = data;
+      }, (err) => {
+        // @ts-ignore
+        this.professor = null;
+      });
     });
   }
 
@@ -121,31 +123,32 @@ export class GetProfessorComponent implements OnInit {
       }
     });
     dialogRef.afterClosed().subscribe(result => {
-
-
       this.professorService.getProfessor(id).subscribe((data: GetProfessorDTO) => {
         this.professor = data;
-
       });
-
     });
   }
 
 //======================================getAllProfessors=========================================================================================================
+
   getAllProfessors() {
     this.professorService.getAllProfessors().subscribe((data: GetProfessorDTO[]) => {
       this.AllProfessors = data;
       this.dataSourceAllProfessors.data = this.AllProfessors;
     }, (err) => {
       this.notifyService.showError(err.error.message);
+      this.dataSourceAllProfessors.data = [];
     });
   }
 
-  removeProfessorForGetAllProfessorsFunction(id: number) {
-    this.professorService.removeProfessor(id).subscribe((data: Professor) => {
+  openDialogRemoveProfessorForGetAllProfessorsFunction(id: number) {
+    const dialogRef = this.dialog.open(RemoveProfessorConfirmationDialogComponent, {
+      data: {
+        professorId: id,
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
       this.getAllProfessors();
-    }, (err) => {
-      this.notifyService.showError(err.error.message);
     });
   }
 
